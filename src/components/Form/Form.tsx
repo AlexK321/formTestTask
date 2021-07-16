db51163acce40cdf5abb52614e22c56c84adad4b
Alexey Kostevich <a.kostevich@itransition.com>
2021-07-16 10:58:30 +0300

    refactoring

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,24 +1,14 @@
 import React, { FC, useState } from 'react';
 import { Form as FormComponent } from 'antd';
 import axios from 'axios';
-import {
-  USER_NAME,
-  PHONE_NUMBER,
-  EMAIL,
-  POST_URL,
-  LABEL_COL,
-  WRAPPER_COL,
-  CORRECT_SMS_CODE,
-} from './constants';
+import { USER_NAME, PHONE_NUMBER, EMAIL, LABEL_COL, WRAPPER_COL, POST_URL } from './constants';
 import './Form.less';
 import SubmitResult from '../SubmitResult/SubmitResult';
-import ErrorState from '../ErrorState/ErrorState';
 import FormItem from '../FormItem/FormItem';
 import SubmitBlock from '../SubmitBlock/SubmitBlock';
 import SMSBlock from '../SMSBlock/SMSBlock';
 import getInitialValues from './getInitialValue';
 import handleChange from './handleChange';
-import sendFormData from './sendFormData';
 import setTimeDelay from './setTimeDelay';
 import useSMSResendTimer from '../../hooks/useSMSResendTimer';
 import { FormData } from './interface';
@@ -31,10 +21,8 @@ const initialValue = {
 let formDataValues: FormData = {};
 
 const Form: FC = () => {
-  const [error, setError] = useState<string | null>('');
   const [hasError, setHasError] = useState<boolean | null>(null);
   const [isSMSItem, setSMSItem] = useState<boolean | null>(false);
-  const [counterSMS, setCounterSMS] = useState<number>(0);
 
   const SMSResendTimer = useSMSResendTimer();
 
@@ -44,26 +32,14 @@ const Form: FC = () => {
     formDataValues = formData;
   };
 
-  const onSubmit = async (SMSCode: string): Promise<void> => {
+  const sendFormData = async (): Promise<void> => {
     try {
-      const response = await axios.post(POST_URL, { SMSCode });
-
-      if (response.data.SMSCode === CORRECT_SMS_CODE) {
-        sendFormData(formDataValues, setHasError);
-      } else {
-        setCounterSMS(counterSMS + 1);
-
-        if (counterSMS === 2) {
-          setSMSItem(false);
-          setCounterSMS(0);
-        }
-      }
+      await axios.post(POST_URL, formDataValues);
+      setHasError(false);
     } catch (err) {
-      setError(err);
+      setHasError(true);
     }
   };
-
-  if (error) return <ErrorState error={error} />;
 
   if (hasError !== null) return <SubmitResult hasError={hasError} />;
 
@@ -84,12 +60,7 @@ const Form: FC = () => {
       {!isSMSItem ? (
         <SubmitBlock SMSResendTimer={SMSResendTimer} />
       ) : (
-        <SMSBlock
-          counterSMS={counterSMS}
-          onSearch={onSubmit}
-          setSMSItem={setSMSItem}
-          setCounterSMS={setCounterSMS}
-        />
+        <SMSBlock setSMSItem={setSMSItem} sendFormData={sendFormData} />
       )}
     </FormComponent>
   );
--- src/components/SMSBlock/SMSBlock.tsx
+++ src/components/SMSBlock/SMSBlock.tsx
@@ -1,23 +1,47 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './SMSBlock.less';
 import { Button } from 'antd';
 import Search from 'antd/lib/input/Search';
 import classNames from 'classnames';
+import axios from 'axios';
+import { CORRECT_SMS_CODE, POST_URL } from '../Form/constants';
+import ErrorState from '../ErrorState/ErrorState';
 
 interface ParamTypes {
-  counterSMS: number;
-  onSearch: (arg0: string) => void;
   setSMSItem: (arg0: boolean) => void;
-  setCounterSMS: (arg0: number) => void;
+  sendFormData: () => Promise<void>;
 }
 
-const SMSBlock: FC<ParamTypes> = ({ counterSMS, onSearch, setSMSItem, setCounterSMS }) => {
+const SMSBlock: FC<ParamTypes> = ({ setSMSItem, sendFormData }) => {
+  const [error, setError] = useState<string | null>('');
+  const [counterSMS, setCounterSMS] = useState<number>(0);
   const SMsBlockClass = classNames('form-row', 'sms-field', { 'error-class': counterSMS > 0 });
+
+  const onSubmit = async (SMSCode: string): Promise<void> => {
+    try {
+      const response = await axios.post(POST_URL, { SMSCode });
+
+      if (response.data.SMSCode === CORRECT_SMS_CODE) {
+        sendFormData();
+      } else {
+        setCounterSMS(counterSMS + 1);
+
+        if (counterSMS === 2) {
+          setSMSItem(false);
+          setCounterSMS(0);
+        }
+      }
+    } catch (err) {
+      setError(err);
+    }
+  };
+
+  if (error) return <ErrorState error={error} />;
 
   return (
     <div className={SMsBlockClass} id="smsField">
       <div>
-        <Search placeholder="SMS-код" enterButton=">" size="large" onSearch={onSearch} />
+        <Search placeholder="SMS-код" enterButton=">" size="large" onSearch={onSubmit} />
         {counterSMS > 0 && (
           <p className="sms-warning">Неправильный код. Осталось {3 - counterSMS} попытки.</p>
         )}
